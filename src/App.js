3f76721a90360f15ea1a709879fe35f458844071
Fathahudheen <vfathahu123@gmail.com>
2023-08-04 10:43:37 +0530

    Repushed due to npm error

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Banner from './Banner'
+import Learn from './Learn'
+import Newsletter from './Newsletter'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Banner/>
+      <Learn/>
+      <Newsletter/>
+    </div>
+  )
+}
+
+export default App
